caff84e0bd62cd9ca0411212c9a7b15a470328e9
khushi173 <khushijamwal173@gmail.com>
2022-03-15 17:06:03 +0530

    Calculator updated using inheritance

--- src/com/harman/batch4/Calculator.java
+++ src/com/harman/batch4/Calculator.java
@@ -3,20 +3,18 @@ package com.harman.batch4;
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
-public class Calculator {
+public class Calculator extends ParentCalculator{
     public static void main(String[] args) {
-        Addition adcalc = new Addition();//Object Creation
-        Subtraction subcalc = new Subtraction();
-        Multiplication mulcalc = new Multiplication();
+        Calculator ob = new Calculator();
         Scanner input = new Scanner(System.in);
         int n1, n2, result1, result2, result3, result4;
         System.out.println("ENTER num1");
         n1 = input.nextInt();
         System.out.println("ENTER num2");
         n2 = input.nextInt();
-        result1 = adcalc.AdditionOperation(n1,n2);
-        result2 = subcalc.SubtractOperation(n1,n2);
-        result3 = mulcalc.MultiplicationOperation(n1,n2);
+        result1 = ob.AdditionOperation(n1,n2);
+        result2 = ob.SubtractOperation(n1,n2);
+        result3 = ob.MultiplicationOperation(n1,n2);
         System.out.println(result1);
         System.out.println(result2);
         System.out.println(result3);
---
+++ src/com/harman/batch4/ParentCalculator.java
@@ -0,0 +1,19 @@
+package com.harman.batch4;
+
+public class ParentCalculator {
+    public int AdditionOperation(int x, int y)
+    {
+        return x+y;
+    }
+
+    public int SubtractOperation(int x, int y)
+    {
+        return x-y;
+    }
+
+    public int MultiplicationOperation(int x, int y)
+    {
+        return x*y;
+    }
+
+}
